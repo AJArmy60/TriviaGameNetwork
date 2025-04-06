4685d7d053d494ef8f574a3019245ef2e5a0e4b6
ConnorRyan313 <crryan3@quinnipiac.edu>
2025-04-06 18:51:09 -0400

    can send UDP from ClientWindow

--- src/ClientWindow.java
+++ src/ClientWindow.java
@@ -12,6 +12,7 @@ import java.util.TimerTask;
 import javax.swing.*;
 
 public class ClientWindow implements ActionListener {
+	private Client client;
     private JButton poll;
     private JButton submit;
     private JRadioButton options[];
@@ -86,6 +87,7 @@ public class ClientWindow implements ActionListener {
             case "Poll":
                 poll.setEnabled(false);  // Disable Poll button
                 submit.setEnabled(true);  // Enable Submit button
+				client.sendUDP(); //send UDP packet to server
                 break;
             case "Submit":
                 handleAnswerSubmission();  // Check if the selected answer is correct and update score
